5283b7dced82336b39650a09adf2b490068cee13
Carlos Baide <ccbaide86@gmail.com>
2024-12-07 18:58:48 -0600

    Agregando validaciones de usuario

---
+++ api-tienda/schemas/user.schemas.js
@@ -0,0 +1,21 @@
+import { z } from 'zod'
+
+const userSchema = z.object(
+    {
+        "nombre": z.string({
+            invalid_type_error: "El nombre debe ser un string"
+        }).trim().min(3, {
+            message: "El nombre debe tener al menos 3 caracteres"
+        }),
+        "correo": z.string().email({
+            message: "El email no es válido"
+        }).endsWith('example.com', {
+            message: "El email debe ser de un dominio válido"
+        }),
+        "contrasena": z.string().trim(),
+        "rol": z.enum(['administrador', 'cliente'])
+
+    },
+).strict()
+
+export const validateUserSchema = (user) => userSchema.safeParse(user)
