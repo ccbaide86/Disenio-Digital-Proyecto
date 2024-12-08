865a1d76994d0bd7854d96b313e9d9bee48b5c92
JafetOrellana <orellanajafet507@gmail.com>
2024-12-07 23:30:26 -0600

    agrando cors para aceptar solicitudes desde cualquier ruta

--- api-tienda/index.js
+++ api-tienda/index.js
@@ -1,4 +1,5 @@
 import express, { json } from "express";
+import cors from "cors"; 
 import productRoute from "./routes/products.js";
 import cartRouter from "./routes/carts.js";
 import paymentRouter from "./routes/payments.js";
@@ -8,26 +9,32 @@ import inventoryRouter from "./routes/inventory.js";
 
 const app = express();
 
+
+app.use(cors({
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+}));
+
 app.disable("x-powered-by");
 app.use(json());
 
 const PORT = process.env.PORT || 3000;
 
-// Rutas 
+// Rutas
 app.use('/products', productRoute);
 app.use('/cart', cartRouter);
 app.use('/payments', paymentRouter);
 app.use('/auth', authRouter);
-app.use('/inventory',inventoryRouter)
+app.use('/inventory', inventoryRouter);
 
-
-// Midleware para manejo de rutas inexistentes 
+// Middleware para manejo de rutas inexistentes
 app.use((req, res) => {
     res.status(404).json({
         error: true,
         message: "Ruta no encontrada"
     });
-})
+});
 
 app.listen(PORT, () => {
     console.log(`Server corriendo en el puerto ${PORT}`);
